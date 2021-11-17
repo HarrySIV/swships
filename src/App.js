c045d1e8c0e7462c0f99e97f0f5a36be928a19e4
Harry Sanders <harry.sandersiv@yahoo.com>
2021-11-16 23:23:07 -0500

    Deploy SW Ships

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
+import styles from './App.module.css';
+import ShipPage from './Components/ShipPage/ShipPage';
+import ShipInfo from './Components/ShipInfo/ShipInfo';
+import InfoBtn from './Components/ShipPage/InfoBtn';
+//import MovieInfo from './Components/MovieInfo/MovieInfo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [starships, setStarships] = useState([]);
+  const [movieList, setMovieList] = useState([]);
+  const [filteredStarship, setFilteredStarships] = useState([]);
+  useEffect(() => {
+    const fetchStarships = () => {
+      fetch('https://swapi.dev/api/starships')
+      .then(response => response.json())
+      .then(data => setStarships(data.results));
+    }
+  fetchStarships();
+  }, []);
 
+  useEffect(() => {
+    const fetchMovieInfo = () => {
+    fetch('https://swapi.dev/api/films')
+    .then(response => response.json())
+    .then(data => setMovieList(data.results));
+    }
+    fetchMovieInfo();
+ }, []);
+
+  const infoBtnToggleRef = useRef();
+  
+  if(!starships.length) return (<h1>...</h1>);
+  //if(!movieList.length) return (<h1>...</h1>);
+
+
+  return (
+    <>
+      {starships.map(ships => (
+        <div className={styles.container} key={ships.created} id={ships.edited}>
+          <ShipPage ships={ships} />
+          <InfoBtn ships={ships}/>
+          {/*<MovieInfo ships={ships} movies={movieList} /> */}
+          <ShipInfo ships={ships} />
+        </div>
+      ))}
+    </>
+  )
+}
 export default App;
